8607a0f3ecb4fa44582fb17639dfc18c45c062ae
Raphael Parreira <rbp@blue-ocean-robotics.com>
2019-12-09 01:56:59 -0300

    questoes 1 e 2 finalizadas

--- mdl_kinovagen3.m
+++ mdl_kinovagen3.m
@@ -24,11 +24,159 @@ links = [
 gen3=SerialLink(links, 'name', 'Kinova Gen3');
 gen3.base=trotx(pi);
 
-%gen3.plotopt = {'workspace', [-1.5 1.5 -1.5 1.5 -1 2], 'jvec'};
-%gen3.plotopt = {'jvec'};
-%gen3.plotopt = {'workspace', [-4 4 -4 4 -4 4], 'jvec', 'jointdiam' 2};
-
 qz = [0 0 0 0 0 0 0];
 qd = [0 41.4 0 90 0 -41.4 0]*pi/180;
 qr = [0 0  0 90 180 0 0]*pi/180;
 
+
+
+save_figure = true;
+plot_figures = true;
+
+if plot_figures
+    figure(1);
+    close(1);
+    gen3.plot(qr, 'floorlevel', -0.1, 'noname', 'notiles');
+    if save_figure
+        export_fig('./latex/figs/ex1_ready', '-pdf', '-painters', '-transparent');
+    end
+end
+
+traj_num = 3;
+control_num = 1;
+
+%jacob_max = 5.229;
+%jacob_max = 4.277;
+%xd_ponto_max = 0.2356;
+%e_max = 0.1417;
+
+%K = ((3/jacob_max) - xd_ponto_max)/e_max
+
+%K = 1;
+
+        switch traj_num
+            case 1 % trajetória a
+            traj_s = 'a_';
+            case 2 % trajetória b
+            traj_s = 'b_';
+            case 3 % trajetória c
+            traj_s = 'c_';
+        end
+        switch control_num
+            case 1 % controle 1
+                ctrl = 1;
+                ctrl_s = '1';
+                alpha = 1;
+                if traj_num == 1
+                    K = 7.24;
+                elseif traj_num == 2
+                    K = 8.57;
+                else
+                    K = 8.57;
+                end
+            case 2 % controle 2a
+                ctrl = -1;
+                ctrl_s = '2';
+                alpha = 0;
+                if traj_num == 1
+                    K = 8.91;
+                elseif traj_num == 2
+                    K = 8.91;
+                else
+                    K = 8.91;
+                end
+        end
+
+sim('kinctrlkinova');
+if plot_figures
+            close(1);
+            figure(1);
+            close(1);
+            %figure(1);
+            %jFrame = get(handle(figure(1)), 'JavaFrame');
+            %pause(1);
+            %jFrame.setMaximized(1);
+            %antro.plot(q0', 'floorlevel', -0.1, 'noname', 'notiles', 'trail', 'b', 'delay', 0, 'noraise')
+            %sq = size(q.signals.values);
+            %for k = 1:sq(3)
+            %   antro.animate(q.Data(:,:,k)');
+            %end
+
+
+            % preparando plots
+            stime = size(x_sist.time);
+            xplot = zeros(1, stime(1));
+            yplot = zeros(1, stime(1));
+            zplot = zeros(1, stime(1));
+
+            % plot x
+            plot3(x_desejado.Data(:,1)', x_desejado.Data(:,2)', x_desejado.Data(:,3)', 'r');
+            hold on;
+            for k = 1:stime(1)
+                xplot(k) = x_sist.signals.values(k, 1);
+                yplot(k) = x_sist.signals.values(k, 2);
+                zplot(k) = x_sist.signals.values(k, 3);
+            end
+            plot3(xplot, yplot, zplot);
+            
+            axis([0.2 0.7 -1.5 1.5 -0.1 0.5])
+            grid on;
+            xlabel('x'); ylabel('y'); zlabel('z');
+            hold off;
+            legend({'$x_d$', '$x$'},'Interpreter','latex');
+
+            if traj_num == 3
+                axis manual;
+                hold on;
+                [X,Y] = meshgrid(-10:.1:10);
+                a=1; b=-1; c=1; d=(0.456 + 0.2);
+                Z=(d- a * X - b * Y)/c;
+                surf(X,Y,Z, C,'LineStyle', 'none', 'FaceAlpha', 0.5, 'EdgeAlpha', 0.5)
+                colormap summer
+                xlabel('x'); ylabel('y'); zlabel('z');
+                hold off;
+                if save_figure
+                    export_fig(['latex/figs/ex1_' traj_s ctrl_s '_x'], '-png', '-opengl');
+                end
+            elseif save_figure
+                export_fig(['latex/figs/ex1_' traj_s ctrl_s '_x'], '-pdf', '-painters', '-transparent');
+            end
+            
+            close(1);
+            axis auto;
+        
+            % plot erro
+            x1plot = zeros(1, stime(1));
+            x2plot = zeros(1, stime(1));
+            x3plot = zeros(1, stime(1));
+            for k = 1:stime(1)
+                x1plot(k) = e.signals.values(k, 1);
+                x2plot(k) = e.signals.values(k, 2);
+                x3plot(k) = e.signals.values(k, 3);
+            end
+            plot(e.time, x1plot, e.time, x2plot, e.time, x3plot);
+            xlabel('time (s)'); ylabel('e (m)');
+            legend('e_1', 'e_2', 'e_3');
+            if save_figure
+                export_fig(['latex/figs/ex1_' traj_s ctrl_s '_e'], '-pdf', '-painters', '-transparent');
+            end
+
+            % plot u
+            u1plot = zeros(1, stime(1));
+            u2plot = zeros(1, stime(1));
+            u3plot = zeros(1, stime(1));
+            u4plot = zeros(1, stime(1));
+            for k = 1:stime(1)
+                u1plot(k) = u.signals.values(k, 1);
+                u2plot(k) = u.signals.values(k, 2);
+                u3plot(k) = u.signals.values(k, 3);
+                u4plot(k) = u.signals.values(k, 4);
+            end
+            plot(e.time, u1plot, e.time, u2plot, e.time, u3plot, e.time, u4plot);
+            xlabel('time (s)'); ylabel('u = $\dot{\theta}$','Interpreter','latex');
+            legend({'$\dot{\theta}_1$', '$\dot{\theta}_2$', '$\dot{\theta}_3$', '$\dot{\theta}_4$'},'Interpreter','latex');
+            
+            if save_figure
+                export_fig(['latex/figs/ex1_' traj_s ctrl_s '_dq'], '-pdf', '-painters', '-transparent');
+            end
+end
--- planar3dof.m
+++ planar3dof.m
@@ -1,6 +1,8 @@
-
-
+clear;
 clear L
+
+save_figure = true;
+plot_figures = true;
 
 L(1) = Link([ 0 0 0.5	0	0], 'standard');
 L(2) = Link([ 0 0 0.5	0	0], 'standard');
@@ -85,4 +87,148 @@ limJun = -limJun/(2*n);
 limJunParc = [diff(limJun, t1); diff(limJun, t2); diff(limJun, t3)];
 matlabFunction(limJun, limJunParc, 'File', 'limJunFunc','Vars', [t1 t2 t3]);
 
-K= 50;
+
+% plot q0
+if plot_figures
+    figure(1);
+    close(1);
+    planar.plot(qz, 'floorlevel', -0.3, 'noname', 'notiles', 'zoom', 0.6);
+    if save_figure
+        export_fig(['latex/figs/ex2_planar_0'], '-pdf', '-painters', '-transparent');
+    end
+    figure(1);
+    close(1);
+    planar.plot(q0, 'floorlevel', -0.3, 'noname', 'notiles');
+    if save_figure
+        export_fig(['latex/figs/ex2_planar_tetha0'], '-pdf', '-painters', '-transparent');
+    end
+end
+
+for i = 1:3 % controle 1, 2, 2a1, 2a100 e 2b1
+    figure(1);
+    close(1);
+    K0 = 0;
+    switch i
+        case 1 % controle 1
+            ctrl = 1
+            ctrl_s = '1';
+            K = 50;
+            sim('planar3dof_sim');
+        case 2 % controle 2
+            ctrl = 2
+            ctrl_s = '2a';
+            K = 50;
+            sim('planar3dof_sim2a');
+        case 3 % controle 2a1
+            ctrl = 3
+            ctrl_s = '2b';
+            K = 50;
+            K0 = 1;
+            sim('planar3dof_sim2b');
+    end
+    
+    if plot_figures
+        figure(1);
+        close(1);
+        
+        % preparando plots
+        stime = size(x.time);
+        xplot = zeros(1, stime(1));
+        yplot = zeros(1, stime(1));
+        zplot = zeros(1, stime(1));
+        
+        % plot trajetória
+        %planar.plot(q.signals.values(:,:,:)', 'floorlevel', -0.3, 'noname', 'notiles');
+        %planar.plot(q.signals.values(1,:));
+        %plot3(q.signals.values(:,1)', q.signals.values(:,2)', q.signals.values(:,3)', 'r');
+
+        % plot x
+        qsize = size(q.signals.values);
+        planar.plot(q.signals.values(qsize(1),:));
+        hold on;
+        plot3(x_desejado.Data(:,1)', x_desejado.Data(:,2)', x_desejado.Data(:,3)', 'r');
+        hold on;
+        for k = 1:stime(1)
+            xplot(k) = x.signals.values(1, k);
+            yplot(k) = x.signals.values(2, k);
+            zplot(k) = x.signals.values(3, k);
+        end
+        plot3(xplot, yplot, zplot);
+        if save_figure
+            export_fig(['latex/figs/ex2_' ctrl_s '_traj'], '-pdf', '-painters', '-transparent');
+        end
+        
+        figure(1);
+        close(1);
+        
+        % plot erro
+        estime = size(e.time);
+        x1plot = zeros(1, stime(1));
+        x2plot = zeros(1, stime(1));
+        x3plot = zeros(1, stime(1));
+        if ctrl == 1
+            x6plot = zeros(1, stime(1));
+        end
+        for j = 1:stime(1)
+            x1plot(j) = e.signals.values(j, 1);
+            x2plot(j) = e.signals.values(j, 2);
+            x3plot(j) = e.signals.values(j, 3);
+        end
+        plot(e.time, x1plot, e.time, x2plot, e.time, x3plot);
+        legend('e x_1 (m)', 'e x_2 (m)', 'e x_3 (m)');
+        xlabel('time (s)'); ylabel('erro');
+        if save_figure
+            export_fig(['latex/figs/ex2_' ctrl_s '_e'], '-pdf', '-painters', '-transparent');
+        end
+        
+        % plot q
+        q1plot = zeros(1, stime(1));
+        q2plot = zeros(1, stime(1));
+        q3plot = zeros(1, stime(1));
+        for k = 1:stime(1)
+            q1plot(k) = q.signals.values(k, 1);
+            q2plot(k) = q.signals.values(k, 2);
+            q3plot(k) = q.signals.values(k, 3);
+        end
+        plot(e.time, q1plot, e.time, q2plot, e.time, q3plot);
+        xlabel('time (s)'); ylabel('u = $\dot{\theta}$','Interpreter','latex');
+        legend({'$\theta_1$', '$\theta_2$', '$\theta_3$'},'Interpreter','latex');
+        if save_figure
+            export_fig(['latex/figs/ex2_' ctrl_s '_q'], '-pdf', '-painters', '-transparent');
+        end
+        
+        % plot dq
+        dq1plot = zeros(1, stime(1));
+        dq2plot = zeros(1, stime(1));
+        dq3plot = zeros(1, stime(1));
+        for k = 1:stime(1)
+            dq1plot(k) = dq.signals.values(k, 1);
+            dq2plot(k) = dq.signals.values(k, 2);
+            dq3plot(k) = dq.signals.values(k, 3);
+        end
+        plot(e.time, dq1plot, e.time, dq2plot, e.time, dq3plot);
+        xlabel('time (s)'); ylabel('u = $\dot{\theta}$','Interpreter','latex');
+        legend({'$\dot{\theta}_1$', '$\dot{\theta}_2$', '$\dot{\theta}_3$'},'Interpreter','latex');
+        if save_figure
+            export_fig(['latex/figs/ex2_' ctrl_s '_dq'], '-pdf', '-painters', '-transparent');
+        end
+        
+        % plot manip
+        if i < 3
+            plot(e.time, manip_val.signals.values);
+            xlabel('time (s)'); ylabel('$\omega(\theta)$','Interpreter','latex');
+            if save_figure
+                export_fig(['latex/figs/ex2_' ctrl_s '_manip'], '-pdf', '-painters', '-transparent');
+            end
+        end
+        
+        % plot limJun
+        if (i > 2)
+            plot(e.time, limJun_val.signals.values);
+            xlabel('time (s)'); ylabel('$\omega(\theta)$','Interpreter','latex');
+            if save_figure
+                export_fig(['latex/figs/ex2_' ctrl_s '_lim_jun'], '-pdf', '-painters', '-transparent');
+            end
+        end
+    end
+end
